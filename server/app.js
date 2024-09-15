3684bf1988b9a44cb15a282d39696792ed172ca4
Saransh Gangwar <91624984+saransh79@users.noreply.github.com>
2024-09-15 15:50:05 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const app= express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://chat-website-eta.vercel.app",
     credentials: true
 }));
 
